96fe526ddec48660cb16c2857d6a52cdadd95116
Elda34 <eldabackend123@gmail.com>
2024-10-10 15:57:55 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-   
-async function connectDB(){
-    try{
-        await mongoose.connect(process.env.MONGODB_URI)
-    }catch(err){
-        console.log(err)
+const connectDB = async () => {
+    try {
+        const uri = process.env.MONGODB_URI; // Ensure this is defined
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to DB');
+    } catch (error) {
+        console.error('Error connecting to DB:', error.message);
+        process.exit(1); // Exit process with failure
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
