6e9a0cc58c24149da56da7ac40124ba26c3829be
Elda34 <eldabackend123@gmail.com>
2024-10-11 09:32:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,17 +29,12 @@ app.use((req, res, next) => {
 // });
 
 if(process.env.NODE_ENV === "production"){
-    app.use(cors({
-        origin: "http://23.20.155.44", // Use your public IP address
-        credentials: true,
-    }));
+    
     app.use(express.static(path.join(__dirname, '../client/build')));
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
-    // })
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+        res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+    })
+  
 }
 
 const PORT = 8080 || process.env.PORT
